dfa0afbbb7a848b8cb54d8fce0cafba26706e3c2
ReziMi <revaz.mikadze92@gmail.com>
2024-05-29 00:42:03 +0400

    category showcase page added

---
+++ src/components/Rating.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Rating() {
+  return (
+    <div>
+        <span className='ratting'>
+            <i className="icofont-ui-rating"></i>
+            <i className="icofont-ui-rating"></i>
+            <i className="icofont-ui-rating"></i>
+            <i className="icofont-ui-rating"></i>
+            <i className="icofont-ui-rating"></i>
+        </span>
+    </div>
+  )
+}
+
+export default Rating
