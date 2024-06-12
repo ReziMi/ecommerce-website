8692f3922afe570ef172eca781f9adc7708a87f0
ReziMi <revaz.mikadze92@gmail.com>
2024-06-13 03:20:26 +0400

    shop page with checkout is completed

--- src/shop/CartPage.jsx
+++ src/shop/CartPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import PageHeader from "../components/PageHeader";
 import { Link } from "react-router-dom";
 import delImgUrl from "../assets/images/shop/del.png";
+import CheckOutPage from "./CheckOutPage";
 
 function CartPage() {
   const [cartItems, setCartItems] = useState([]);
@@ -162,7 +163,9 @@ function CartPage() {
 
                 <form className="cart-checkout">
                   <input type="submit" value="Update Cart" />
-                  <div>CheckoutPage</div>
+                  <div>
+                    <CheckOutPage/>
+                  </div>
                 </form>
               </div>
 
